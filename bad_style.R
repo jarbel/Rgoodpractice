e0c35bbd201dd4a0ae0ab20258995f019d7648d1
Julyan Arbel <jarbel@MacBook-Pro-de-Julyan.local>
2018-01-31 14:15:19 +0100

    edits to pres and bad_style.R

--- bad_style.R
+++ bad_style.R
@@ -1,15 +1,12 @@
-# Spacing
-
+# Spacing 
 average<-mean(feet/12 + inches,na.rm=TRUE)
 sqrt(x ^ 2 + y ^ 2)
 x <- 1 : 10
 base :: get
 
 # Indenting
-
 if (y < 0 && debug)
-message("Y is negative")
+  message("Y is negative")
 
 # Assignment
-
 x = 5
